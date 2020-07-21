8efbd4281876127c8a489405a38096e45431212c
Richie <richieyang9111@gmail.com>
2020-07-21 19:49:16 +0800

    First commit to testing branch

--- src/algorithm/Recommendation.java
+++
@@ -1,9 +0,0 @@
-package algorithm;
-
-import java.util.List;
-
-import entity.Item;
-
-public interface Recommendation {
-  public List<Item> recommendItems(String userId, double latitude, double longitude);
-}
